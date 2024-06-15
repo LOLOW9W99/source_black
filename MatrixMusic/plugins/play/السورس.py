d0f687a31debb237a16304c9579f35b03f13e737
LOLOW9W99 <asasynyy@gmail.com>
2024-06-16 00:20:35 +0200

    Update السورس.py

--- MatrixMusic/plugins/play/السورس.py
+++ MatrixMusic/plugins/play/السورس.py
@@ -31,14 +31,14 @@ from pyrogram.errors import FloodWait
 async def huhh(client: Client, message: Message):
     await message.reply_video(
         video=f"https://graph.org/file/fb68ae3b5a2e1d62fd94f.mp4",
-        caption=f"""• [⌯𝐃𝐄𝐕.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/O_U_S1) •\n
+        caption=f"""• [⌯𝐃𝐄𝐕.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/O_U_1S) •\n
  [⌯𝐒𝐎𝐔𝐑𝐂𝐄 𝗕𝗹𝗮𝗰𝗸⌯](https://t.me/O_U_Q1)\n
  [⌯𝐒𝐔𝐏𝐏𝐔𝐑𝐓.𝐒𝐎𝐔𝐑𝐂𝐄⌯](https://t.me/Senzir_Suuport)\n""",
         reply_markup=InlineKeyboardMarkup(
         [
             [
                     InlineKeyboardButton(
-                        "‹ 𝐃𝐄𝐕 𝐒𝐎𝐔𝐑𝐂𝐄 › ", url=f"https://t.me/O_U_S1"),
+                        "‹ 𝐃𝐄𝐕 𝐒𝐎𝐔𝐑𝐂𝐄 › ", url=f"https://t.me/O_U_1S"),
                 ],[
                     InlineKeyboardButton(
                         "‹ ᏟᎻᎪΝΝᎬᏞ ›", url=f"https://t.me/O_U_Q1"), 
@@ -57,7 +57,7 @@ async def huhh(client: Client, message: Message):
 async def huhh(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://graph.org/file/a52bd3bd88fffadf20aca.jpg",
-        caption=f"""• ⌯ Developer Name : ˛ 𝐇 𝐌 𝐃 ⌯ •\n- Devloper Username : @O_U_S1 Devloper id : 6526392564 ⋅""",
+        caption=f"""• ⌯ Developer Name : ˛ 𝐇 𝐌 𝐃 ⌯ •\n- Devloper Username : @O_U_1S Devloper id : 6526392564 ⋅""",
         reply_markup=InlineKeyboardMarkup(
         [
             [
@@ -65,7 +65,7 @@ async def huhh(client: Client, message: Message):
                         " ‹ قناة السورس ⋅ › ", url=f"https://t.me/O_U_Q1"),
                 ],[
                     InlineKeyboardButton(
-                        "𝐇 𝐌 𝐃", url=f"https://t.me/O_U_S1"), 
+                        "𝐇 𝐌 𝐃", url=f"https://t.me/O_U_1S"), 
                 ],[
                     InlineKeyboardButton(
                         "‹ اضف بوت السورس الي مجموعتك ⚡️🎸 ⋅ ›", url=f"http://t.me/@6Z_bot?startgroup=new"),
